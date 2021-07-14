7f16afe4eaa2aa75d50ba7da2a0dd0b8d392747b
eliwh <34358650+eliwh@users.noreply.github.com>
2021-07-14 09:12:36 -0400

    Initial Push
    YO G, this is the first Push, like I said only a few lines to get started lol

---
+++ app.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.bestbuy.com/site/searchpage.jsp?st=the+legend+of+zelda&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys")
