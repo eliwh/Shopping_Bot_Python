5ba230e0ee6b4534aad6aa490bf834001067641c
gglasco <84289722+gglasco@users.noreply.github.com>
2021-07-14 12:27:04 -0400

    Got it to open page, click button.

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+#from selenium.webdriver.common.keys import keys
 
-driver = webdriver.Chrome()
-driver.get("https://www.bestbuy.com/site/searchpage.jsp?st=the+legend+of+zelda&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys")
+driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver')
+driver.get("https://www.bestbuy.com/site/the-legend-of-zelda-skyward-sword-hd-nintendo-switch-lite-nintendo-switch/6414119.p?skuId=6414119")
+
+addtocart = driver.find_element_by_class_name("btn-lg")
+
+addtocart.click()
+
+#gotocart = driver.find_element_by_class_name("c-button-secondary")
+
+#refresh the page to get rid of the JS popup kekw
+driver.refresh()
+
+#cart is a link and not a button..
+#maybe try getting a new page? does it retain that there is something in the cart?
+
+cart = webdriver.Chrome('C:\chromedriver_win32\chromedriver')
+cart.get("https://www.bestbuy.com/cart")
